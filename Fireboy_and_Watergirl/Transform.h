ff58fe92e258b69668aa9a3e95097fb34bb43c1d
nhuhieu193 <mama82bells@gmail.com>
2024-04-19 11:04:02 +0700

    fix bug when jumping

--- Fireboy_and_Watergirl/Transform.h
+++ Fireboy_and_Watergirl/Transform.h
@@ -13,27 +13,36 @@
 class Transform {
 public:
     bool MoveInMap(Vector2D vect) {
+        MoveHorizontal(vect);
+        return MoveVertical(vect);
+    }
+
+    void MoveHorizontal(Vector2D vect) {
         int x = X , y = Y;
-        int UP = Engine::GetInstance() -> CurrentMap -> Up[x][y];
-        int DOWN = Engine::GetInstance() -> CurrentMap -> Down[x][y];
         int LEFT = Engine::GetInstance() -> CurrentMap -> Left[x][y];
         int RIGHT = Engine::GetInstance() -> CurrentMap -> Right[x][y];
-        for (int i = x ; i < x + CHARACTER_WIDTH ; i++) {
-            maximize(UP , Engine::GetInstance() -> CurrentMap -> Up[i][y]);
-            minimize(DOWN , Engine::GetInstance() -> CurrentMap -> Down[i][y]);
-        }
         for (int j = y ; j < y + CHARACTER_HEIGHT ; j++) {
             maximize(LEFT , Engine::GetInstance() -> CurrentMap -> Left[x][j]);
             minimize(RIGHT , Engine::GetInstance() -> CurrentMap -> Right[x][j]);
         }
-        X += vect.X; Y += vect.Y;
-        bool Collide = false;
-        if (Y < UP || Y > DOWN - CHARACTER_HEIGHT) Collide = true;
-
-        maximize(Y , UP);
-        minimize(Y , DOWN - CHARACTER_HEIGHT);
+        X += vect.X;
         maximize(X , LEFT);
         minimize(X , RIGHT - CHARACTER_WIDTH);
+    }
+
+    bool MoveVertical(Vector2D vect) {
+        int x = X , y = Y;
+        int UP = Engine::GetInstance() -> CurrentMap -> Up[x][y];
+        int DOWN = Engine::GetInstance() -> CurrentMap -> Down[x][y];
+        for (int i = x ; i < x + CHARACTER_WIDTH ; i++) {
+            maximize(UP , Engine::GetInstance() -> CurrentMap -> Up[i][y]);
+            minimize(DOWN , Engine::GetInstance() -> CurrentMap -> Down[i][y]);
+        }
+        Y += vect.Y;
+        bool Collide = false;
+        if (Y < UP || Y > DOWN - CHARACTER_HEIGHT) Collide = true;
+        maximize(Y , UP);
+        minimize(Y , DOWN - CHARACTER_HEIGHT);
 
         return Collide;
     }
